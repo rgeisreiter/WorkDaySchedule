e7fceae72271a8423527dd350d3d5351b0daa78e
rgeisreiter <rgeisreiter@yahoo.com>
2021-04-28 16:05:44 -0600

    adjusted html and added var to JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
+  //everything inside this function
+  //displays date
   $("#currentDay").text(moment().format("dddd, MMMM, Do"));
+
+  //listen for save button click
+  $(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val(); //
+    //grab textarea by id=hour-//#
+    //local storage.setitem
+    //time and input in local storage
+    // jquery parents ***
+  });
 });
